7eb3bbed5e73698d58d069bfae1c21c1c42adee3
David Munkacsi <david.munkacsi93@gmail.com>
2021-03-10 21:11:39 +0100

    Present recipes.

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
+  recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl: 'https://www.dessertfortwo.com/wp-content/uploads/2014/12/Schnitzel-2.jpg',
+      ingredients: [ 'French Fries', 'Pork Meat', 'Salad']
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl: 'https://smashedpeasandcarrots.com/wp-content/uploads/2018/03/Instant-Pot-Spaghetti7.jpg',
+      ingredients: [ 'Spaghetti', 'Tomato', 'Basil']
+    },
+  ]
 
   constructor() { }
 
