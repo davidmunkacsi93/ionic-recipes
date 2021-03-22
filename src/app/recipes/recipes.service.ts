1769eee9e046e64557eecb1f79f0053cafb471c3
David Munkacsi <david.munkacsi93@gmail.com>
2021-03-22 20:40:35 +0100

    Move recipes state into recipes service.

---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+  recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl: 'https://www.dessertfortwo.com/wp-content/uploads/2014/12/Schnitzel-2.jpg',
+      ingredients: [ 'French Fries', 'Pork Meat', 'Salad']
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl: 'https://smashedpeasandcarrots.com/wp-content/uploads/2018/03/Instant-Pot-Spaghetti7.jpg',
+      ingredients: [ 'Spaghetti', 'Tomato', 'Basil']
+    },
+  ]
+
+  constructor() { }
+
+  getAllRecipes() {
+    return [... this.recipes];
+  }
+
+  getRecipe(recipeId: string) {
+    return { ... this.recipes.find(r => r.id === recipeId) };
+  }
+}
